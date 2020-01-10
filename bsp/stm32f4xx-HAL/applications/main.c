e6d3533469c057e585dcd6bb608827ccaee1e0a9
Windows VIGA <1610478247@qq.com>
2020-01-10 22:58:07 +0800

    first commit, git clone rtthread on May 6th, it have the STMF4ZGT6 which I'm familiar with, I just added some uarts and change the baud rate to 9600, test the console device successfully.

---
+++ bsp/stm32f4xx-HAL/applications/main.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2015-07-29     Arda.Fu      first implementation
+ */
+#include <rtthread.h>
+#include <board.h>
+
+int main(void)
+{
+    /* user app entry */
+
+	rt_kprintf("my_usart2 now starts!\r\n");
+	
+    return 0;
+}
